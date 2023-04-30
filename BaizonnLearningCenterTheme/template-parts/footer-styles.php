4388d4da887d601b7d5303cbb4bed4175e502ad3
Tianyi2 <tianyi.zhang@my.jcu.edu.au>
2023-04-30 18:21:04 +0800

    Upload baizonn learning center theme

---
+++ BaizonnLearningCenterTheme/template-parts/footer-styles.php
@@ -0,0 +1,64 @@
+<!-- footer styles -->
+
+<style>.u-footer .u-sheet-1 {
+  min-height: 149px;
+}
+.u-footer .u-social-icons-1 {
+  height: 38px;
+  min-height: 16px;
+  width: 182px;
+  min-width: 94px;
+  white-space: nowrap;
+  margin: 24px auto 0;
+}
+.u-footer .u-icon-1 {
+  color: rgb(59, 89, 152) !important;
+}
+.u-footer .u-icon-2 {
+  color: rgb(85, 172, 238) !important;
+}
+.u-footer .u-icon-3 {
+  color: rgb(197, 54, 164) !important;
+}
+.u-footer .u-icon-4 {
+  color: rgb(0, 122, 185) !important;
+}
+.u-footer .u-text-1 {
+  width: 530px;
+  font-size: 0.875rem;
+  margin: 10px auto 0;
+}
+.u-footer .u-text-2 {
+  width: 530px;
+  font-size: 0.875rem;
+  margin: 0 auto 24px;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-social-icons-1 {
+    margin-left: 420px;
+  }
+  .u-footer .u-text-1 {
+    width: 437px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-social-icons-1 {
+    margin-left: auto;
+  }
+  .u-footer .u-text-1 {
+    width: 335px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-text-1 {
+    width: 314px;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-text-1 {
+    width: 263px;
+  }
+  .u-footer .u-text-2 {
+    width: 340px;
+  }
+}</style>
